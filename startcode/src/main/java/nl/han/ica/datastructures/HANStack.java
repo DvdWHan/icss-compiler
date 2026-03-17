c9c498d431d06b609cbc28d6a942a20d3731abaf
DvdWHan <150020220+DvdWHan@users.noreply.github.com>
2026-03-17 17:54:59 +0100

    Implement HANStack

---
+++ startcode/src/main/java/nl/han/ica/datastructures/HANStack.java
@@ -0,0 +1,46 @@
+package nl.han.ica.datastructures;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.EmptyStackException;
+
+@NoArgsConstructor
+public class HANStack<T> implements IHANStack<T> {
+  private Node<T> top = null;
+  private int size = 0;
+
+  @Override
+  public T peek() {
+    ensureNotEmpty();
+    return top.value;
+  }
+
+  @Override
+  public void push(T value) {
+    top = new Node<>(value, top);
+    ++size;
+  }
+
+  @Override
+  public T pop() {
+    T result = peek();
+    Node<T> newTop = top.next;
+    top.next = null;
+    top = newTop;
+    --size;
+    return result;
+  }
+
+  @AllArgsConstructor
+  private static class Node<T> {
+    public T value;
+    public Node<T> next;
+  }
+
+  private void ensureNotEmpty() {
+    if (size == 0) {
+      throw new EmptyStackException();
+    }
+  }
+}
