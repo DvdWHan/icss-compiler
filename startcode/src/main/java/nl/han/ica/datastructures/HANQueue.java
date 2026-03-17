dcf2c962f5b95b5aa4d15c93781b964e5a16a6d6
DvdWHan <150020220+DvdWHan@users.noreply.github.com>
2026-03-17 18:26:06 +0100

    Implement HANQueue

---
+++ startcode/src/main/java/nl/han/ica/datastructures/HANQueue.java
@@ -0,0 +1,71 @@
+package nl.han.ica.datastructures;
+
+import lombok.AllArgsConstructor;
+
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class HANQueue<T> implements IHANQueue<T> {
+  private Node<T> head;
+  private Node<T> tail;
+  private int size;
+
+  @Override
+  public void clear() {
+    head = null;
+    tail = null;
+    size = 0;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  @Override
+  public int getSize() {
+    return size;
+  }
+
+  @Override
+  public T peek() {
+    return head != null ? head.value : null;
+  }
+
+  @Override
+  public void enqueue(T value) {
+    if (isEmpty()) {
+      head = tail = new Node<>(value, null);
+    } else {
+      tail.next = new Node<>(value, null);
+      tail = tail.next;
+    }
+    ++size;
+  }
+
+  @Override
+  public T dequeue() {
+    ensureNotEmpty();
+    T result = peek();
+    head = head.next;
+    --size;
+    return result;
+  }
+
+  @AllArgsConstructor
+  private static class Node<T> {
+    private T value;
+    private Node<T> next;
+  }
+
+  private void ensureNotEmpty() {
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    }
+  }
+
+  public static void main(String[] args) {
+    var q =  new HANQueue<String>();
+    System.out.println(q.peek());
+  }
+}
