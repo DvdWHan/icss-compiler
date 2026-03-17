6db85af46771fd8bada2269ba391724ca7cff149
DvdWHan <150020220+DvdWHan@users.noreply.github.com>
2026-03-17 21:55:17 +0100

    Created .bat files to build and run

--- startcode/build.bat
+++ startcode/build.bat
@@ -1,1 +1,12 @@
 mvn clean compile
+
+set package_name=nl/han/ica/generated
+set output_directory=src/main/java/%package_name%
+set grammar_file=src/main/antlr4/nl/han/ica/icss/parser/ICSS.g4
+
+antlr4 -o %output_directory% -package %package_name% %grammar_file%
+antlr4 -visitor -o %output_directory% -package %package_name% -Dlanguage=Java %grammar_file%
+
+for %%f in (%output_directory%\*) do (
+    if not "%%~xf"==".java" del "%%f"
+)
---
+++ startcode/run.bat
@@ -0,0 +1,4 @@
+set grammar_file=src/main/antlr4/nl/han/ica/icss/parser/ICSS.g4
+set class=stylesheet
+
+java -cp "%ANTLR4_JAR%" org.antlr.v4.gui.Interpreter %grammar_file% %class% -gui
