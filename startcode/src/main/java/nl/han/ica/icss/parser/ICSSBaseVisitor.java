2992d30c899516493f8824c00045711fe11f31b3
DvdWHan <150020220+DvdWHan@users.noreply.github.com>
2026-03-18 09:03:02 +0100

    Moved generated files to parser directory

---
+++ startcode/src/main/java/nl/han/ica/icss/parser/ICSSBaseVisitor.java
@@ -0,0 +1,85 @@
+// Generated from src/main/antlr4/nl/han/ica/icss/parser/ICSS.g4 by ANTLR 4.13.2
+package nl.han.ica.icss.parser;
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+
+/**
+ * This class provides an empty implementation of {@link ICSSVisitor},
+ * which can be extended to create a visitor which only needs to handle a subset
+ * of the available methods.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+@SuppressWarnings("CheckReturnValue")
+public class ICSSBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements ICSSVisitor<T> {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitStylesheet(ICSSParser.StylesheetContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitRuleset(ICSSParser.RulesetContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitSelector(ICSSParser.SelectorContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitElementSelector(ICSSParser.ElementSelectorContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitIdSelector(ICSSParser.IdSelectorContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitClassSelector(ICSSParser.ClassSelectorContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitDeclarations(ICSSParser.DeclarationsContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitDeclaration(ICSSParser.DeclarationContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitProperty(ICSSParser.PropertyContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitValue(ICSSParser.ValueContext ctx) { return visitChildren(ctx); }
+}
---
+++ startcode/src/main/java/nl/han/ica/icss/parser/ICSSListener.java
@@ -0,0 +1,110 @@
+// Generated from src/main/antlr4/nl/han/ica/icss/parser/ICSS.g4 by ANTLR 4.13.2
+package nl.han.ica.icss.parser;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link ICSSParser}.
+ */
+public interface ICSSListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link ICSSParser#stylesheet}.
+	 * @param ctx the parse tree
+	 */
+	void enterStylesheet(ICSSParser.StylesheetContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ICSSParser#stylesheet}.
+	 * @param ctx the parse tree
+	 */
+	void exitStylesheet(ICSSParser.StylesheetContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ICSSParser#ruleset}.
+	 * @param ctx the parse tree
+	 */
+	void enterRuleset(ICSSParser.RulesetContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ICSSParser#ruleset}.
+	 * @param ctx the parse tree
+	 */
+	void exitRuleset(ICSSParser.RulesetContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ICSSParser#selector}.
+	 * @param ctx the parse tree
+	 */
+	void enterSelector(ICSSParser.SelectorContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ICSSParser#selector}.
+	 * @param ctx the parse tree
+	 */
+	void exitSelector(ICSSParser.SelectorContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ICSSParser#elementSelector}.
+	 * @param ctx the parse tree
+	 */
+	void enterElementSelector(ICSSParser.ElementSelectorContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ICSSParser#elementSelector}.
+	 * @param ctx the parse tree
+	 */
+	void exitElementSelector(ICSSParser.ElementSelectorContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ICSSParser#idSelector}.
+	 * @param ctx the parse tree
+	 */
+	void enterIdSelector(ICSSParser.IdSelectorContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ICSSParser#idSelector}.
+	 * @param ctx the parse tree
+	 */
+	void exitIdSelector(ICSSParser.IdSelectorContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ICSSParser#classSelector}.
+	 * @param ctx the parse tree
+	 */
+	void enterClassSelector(ICSSParser.ClassSelectorContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ICSSParser#classSelector}.
+	 * @param ctx the parse tree
+	 */
+	void exitClassSelector(ICSSParser.ClassSelectorContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ICSSParser#declarations}.
+	 * @param ctx the parse tree
+	 */
+	void enterDeclarations(ICSSParser.DeclarationsContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ICSSParser#declarations}.
+	 * @param ctx the parse tree
+	 */
+	void exitDeclarations(ICSSParser.DeclarationsContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ICSSParser#declaration}.
+	 * @param ctx the parse tree
+	 */
+	void enterDeclaration(ICSSParser.DeclarationContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ICSSParser#declaration}.
+	 * @param ctx the parse tree
+	 */
+	void exitDeclaration(ICSSParser.DeclarationContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ICSSParser#property}.
+	 * @param ctx the parse tree
+	 */
+	void enterProperty(ICSSParser.PropertyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ICSSParser#property}.
+	 * @param ctx the parse tree
+	 */
+	void exitProperty(ICSSParser.PropertyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ICSSParser#value}.
+	 * @param ctx the parse tree
+	 */
+	void enterValue(ICSSParser.ValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ICSSParser#value}.
+	 * @param ctx the parse tree
+	 */
+	void exitValue(ICSSParser.ValueContext ctx);
+}
