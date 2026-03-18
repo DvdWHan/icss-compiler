8bab3816c49c5c975b3c8335a740704bfe0e2c32
DvdWHan <150020220+DvdWHan@users.noreply.github.com>
2026-03-18 21:44:47 +0100

    Removed manually generated Icss files that conflicted (duplicate definitions)

--- startcode/src/main/java/nl/han/ica/icss/Pipeline.java
+++ startcode/src/main/java/nl/han/ica/icss/Pipeline.java
@@ -1,12 +1,14 @@
 package nl.han.ica.icss;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import nl.han.ica.icss.ast.Ast;
 import nl.han.ica.icss.checker.Checker;
 import nl.han.ica.icss.checker.SemanticError;
 import nl.han.ica.icss.generator.Generator;
 import nl.han.ica.icss.parser.AstListener;
-import nl.han.ica.icss.parser.ICSSLexer;
-import nl.han.ica.icss.parser.ICSSParser;
+import nl.han.ica.icss.parser.IcssLexer;
+import nl.han.ica.icss.parser.IcssParser;
 import nl.han.ica.icss.transforms.Evaluator;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.atn.ATNConfigSet;
@@ -19,125 +21,113 @@ import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.List;
 
+@Getter
+@NoArgsConstructor
 public class Pipeline implements ANTLRErrorListener {
+  private Ast ast = null;
+  private boolean parsed = false;
+  private boolean checked = false;
+  private boolean transformed = false;
+  private final List<String> errors = new ArrayList<>();
 
-    private Ast ast;
-    private boolean parsed = false;
-    private boolean checked = false;
-    private boolean transformed = false;
-    private List<String> errors;
+  public void parseString(String input) {
 
-    public Pipeline() {
-        errors = new ArrayList<>();
+    //Lex (with Antlr's generated lexer)
+    CharStream inputStream = CharStreams.fromString(input);
+    IcssLexer lexer = new IcssLexer(inputStream);
+    lexer.removeErrorListeners();
+    lexer.addErrorListener(this);
+    errors.clear();
+    try {
+      CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+      //Parse (with Antlr's generated parser)
+      IcssParser parser = new IcssParser(tokens);
+      parser.removeErrorListeners();
+      parser.addErrorListener(this);
+
+      ParseTree parseTree = parser.stylesheet();
+
+      //Extract AST from the Antlr parse tree
+      AstListener listener = new AstListener();
+      ParseTreeWalker walker = new ParseTreeWalker();
+      walker.walk(listener, parseTree);
+
+      this.ast = listener.getAst();
+
+    } catch (RecognitionException e) {
+      this.ast = new Ast();
+      errors.add(e.getMessage());
+
+    } catch (ParseCancellationException e) {
+      this.ast = new Ast();
+      errors.add("Syntax error");
+    }
+    parsed = errors.isEmpty();
+    checked = transformed = false;
+  }
+
+  public boolean check() {
+    if (ast == null) {
+      return false;
     }
 
-    public Ast getAST() {
-        return ast;
-    }
-    public List<String> getErrors() {
-        return errors;
-    }
-    public boolean isParsed() {
-        return parsed;
-    }
-    public boolean isChecked() {
-        return checked;
-    }
-    public boolean isTransformed() {
-        return transformed;
+    (new Checker()).check(this.ast);
+
+    List<SemanticError> errors = this.ast.getErrors();
+    if (!errors.isEmpty()) {
+      for (SemanticError e : errors) {
+        this.errors.add(e.toString());
+      }
     }
 
-    public void parseString(String input) {
+    checked = errors.isEmpty();
+    transformed = false;
+    return errors.isEmpty();
+  }
 
-        //Lex (with Antlr's generated lexer)
-        CharStream inputStream = CharStreams.fromString(input);
-        ICSSLexer lexer = new ICSSLexer(inputStream);
-        lexer.removeErrorListeners();
-        lexer.addErrorListener(this);
-        errors.clear();
-        try {
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
+  public void clearErrors() {
+    errors.clear();
+  }
 
-            //Parse (with Antlr's generated parser)
-            ICSSParser parser = new ICSSParser(tokens);
-            parser.removeErrorListeners();
-            parser.addErrorListener(this);
-
-            ParseTree parseTree = parser.stylesheet();
-
-            //Extract AST from the Antlr parse tree
-            AstListener listener = new AstListener();
-            ParseTreeWalker walker = new ParseTreeWalker();
-            walker.walk(listener, parseTree);
-
-            this.ast = listener.getAst();
-
-        } catch (RecognitionException e) {
-            this.ast = new Ast();
-            errors.add(e.getMessage());
-
-        } catch (ParseCancellationException e) {
-            this.ast = new Ast();
-            errors.add("Syntax error");
-        }
-        parsed = errors.isEmpty();
-        checked = transformed = false;
-    }
-    public boolean check() {
-            if(ast == null)
-                return false;
-
-           (new Checker()).check(this.ast);
-
-            ArrayList<SemanticError> errors = this.ast.getErrors();
-            if (!errors.isEmpty()) {
-                for (SemanticError e : errors) {
-                    this.errors.add(e.toString());
-                }
-            }
-
-            checked = errors.isEmpty();
-            transformed = false;
-            return errors.isEmpty();
+  public void transform() {
+    if (ast == null) {
+      return;
     }
 
-    public void clearErrors(){
-        errors.clear();
-    }
-
-    public void transform() {
-        if(ast == null)
-            return;
-
-        (new Evaluator()).apply(ast);
+    (new Evaluator()).apply(ast);
 
 
-        transformed = errors.isEmpty();
-    }
-    public String generate() {
-        Generator generator = new Generator();
-        return generator.generate(ast);
-    }
+    transformed = errors.isEmpty();
+  }
 
-    //Catch ANTLR errors
-    @Override
-    public void reportAmbiguity(Parser arg0, DFA arg1, int arg2, int arg3,
-                                boolean arg4, BitSet arg5, ATNConfigSet arg6) {
-    }
+  public String generate() {
+    Generator generator = new Generator();
+    return generator.generate(ast);
+  }
 
-    @Override
-    public void reportAttemptingFullContext(Parser arg0, DFA arg1, int arg2,
-                                            int arg3, BitSet arg4, ATNConfigSet arg5) {
-    }
+  //Catch ANTLR errors
+  @Override
+  public void reportAmbiguity(Parser arg0, DFA arg1, int arg2, int arg3, boolean arg4, BitSet arg5, ATNConfigSet arg6) {
+  }
 
-    @Override
-    public void reportContextSensitivity(Parser arg0, DFA arg1, int arg2,
-                                         int arg3, int arg4, ATNConfigSet arg5) {
-    }
+  @Override
+  public void reportAttemptingFullContext(Parser arg0, DFA arg1, int arg2, int arg3, BitSet arg4, ATNConfigSet arg5) {
+  }
 
-    @Override
-    public void syntaxError(Recognizer<?, ?> arg0, Object arg1, int arg2,
-                            int arg3, String arg4, RecognitionException arg5) {
-        errors.add("Syntax error: " + arg4);
-    }
+  @Override
+  public void reportContextSensitivity(Parser arg0, DFA arg1, int arg2, int arg3, int arg4, ATNConfigSet arg5) {
+  }
+
+  @Override
+  public void syntaxError(
+      Recognizer<?, ?> arg0,
+      Object arg1,
+      int arg2,
+      int arg3,
+      String arg4,
+      RecognitionException arg5
+  ) {
+    errors.add("Syntax error: " + arg4);
+  }
 }
