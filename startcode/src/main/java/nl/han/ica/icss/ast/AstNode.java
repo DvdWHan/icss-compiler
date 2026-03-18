968b4f46c2ad838ca02992ff1ee01b438562d11e
DvdWHan <150020220+DvdWHan@users.noreply.github.com>
2026-03-18 21:46:36 +0100

    Implemented WeakHashMap to give every AstNode its own AstNode.ErrorHandler

--- startcode/src/main/java/nl/han/ica/icss/ast/AstNode.java
+++ startcode/src/main/java/nl/han/ica/icss/ast/AstNode.java
@@ -1,13 +1,49 @@
 package nl.han.ica.icss.ast;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import nl.han.ica.icss.checker.SemanticError;
+
 import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
 
 public interface AstNode {
+  Map<AstNode, ErrorHandler> ERROR_HANDLERS = new WeakHashMap<>();
+
   List<AstNode> getChildren();
 
   AstNode addChild(AstNode child);
 
   default String getNodeLabel() {
     return getClass().getSimpleName();
+  }
+
+  default ErrorHandler getErrorHandler() {
+    return ERROR_HANDLERS.computeIfAbsent(this, k -> new ErrorHandler());
+  }
+
+  default SemanticError getError() {
+    return getErrorHandler().getError();
+  }
+
+  default void setError(SemanticError error) {
+    getErrorHandler().setError(error);
+  }
+
+  default boolean hasError() {
+    return getErrorHandler().hasError();
+  }
+
+  @Getter
+  @Setter
+  @NoArgsConstructor
+  final class ErrorHandler {
+    private SemanticError error = null;
+
+    public boolean hasError() {
+      return error != null;
+    }
   }
 }
