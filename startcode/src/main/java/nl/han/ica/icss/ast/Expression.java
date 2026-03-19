1b89a239ce18a6426525834a0a8d64d3c5830833
DvdWHan <150020220+DvdWHan@users.noreply.github.com>
2026-03-19 18:45:32 +0100

    Refactored ExpressionType into Expression.Type

--- startcode/src/main/java/nl/han/ica/icss/ast/Expression.java
+++ startcode/src/main/java/nl/han/ica/icss/ast/Expression.java
@@ -1,3 +1,13 @@
 package nl.han.ica.icss.ast;
 
-public interface Expression extends AstNode {}
+public interface Expression extends AstNode {
+  enum Type {
+    BOOLEAN,
+    COLOR,
+    PERCENTAGE,
+    PIXEL,
+    SCALAR,
+    UNDEFINED,
+    VARIABLE,
+  }
+}
--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -2,20 +2,19 @@ package nl.han.ica.icss.checker;
 
 import nl.han.ica.datastructures.IHanLinkedList;
 import nl.han.ica.icss.ast.Ast;
-import nl.han.ica.icss.ast.types.ExpressionType;
+import nl.han.ica.icss.ast.Expression;
 
 import java.util.HashMap;
 
 
-
 public class Checker {
 
-    private IHanLinkedList<HashMap<String, ExpressionType>> variableTypes;
+  private IHanLinkedList<HashMap<String, Expression.Type>> variableTypes;
 
-    public void check(Ast ast) {
-        // variableTypes = new HANLinkedList<>();
+  public void check(Ast ast) {
+    // variableTypes = new HANLinkedList<>();
 
-    }
+  }
 
-    
+
 }
