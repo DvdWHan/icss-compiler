5080efed829d7645b2d9a0b0995b1f9d0536a1c5
DvdWHan <150020220+DvdWHan@users.noreply.github.com>
2026-03-20 08:13:36 +0100

    Added variable assignments to Ruleset

--- startcode/src/main/java/nl/han/ica/icss/ast/Ruleset.java
+++ startcode/src/main/java/nl/han/ica/icss/ast/Ruleset.java
@@ -13,7 +13,8 @@ import java.util.List;
 @AllArgsConstructor
 public class Ruleset implements AstNode {
   private Selector selector = null;
-  private final List<Declaration> declarations = new ArrayList<>();
+  private final List<AstNode> variableAssignments = new ArrayList<>();
+  private final List<AstNode> declarations = new ArrayList<>();
 
   @Override
   public List<AstNode> getChildren() {
@@ -28,6 +29,8 @@ public class Ruleset implements AstNode {
   public AstNode addChild(AstNode child) {
     if (child instanceof Selector selector) {
       this.selector = selector;
+    } else if (child instanceof VariableAssignment variableAssignment) {
+      this.variableAssignments.add(child);
     } else if (child instanceof Declaration declaration) {
       this.declarations.add(declaration);
     }
